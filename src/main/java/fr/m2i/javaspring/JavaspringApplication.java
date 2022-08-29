c9a5bde40452b322ab6c34e15f0a32b313710b26
a rahni <rahniahmed@gmail.com>
2022-08-29 12:38:30 +0200

    profiles configuration

--- src/main/java/fr/m2i/javaspring/JavaspringApplication.java
+++ src/main/java/fr/m2i/javaspring/JavaspringApplication.java
@@ -1,13 +1,18 @@
 package fr.m2i.javaspring;
 
+import fr.m2i.javaspring.config.DataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class JavaspringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaspringApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(JavaspringApplication.class, args);
+                
+                DataSourceConfig dataConfig = ctx.getBean(DataSourceConfig.class);
+                dataConfig.setup();
 	}
 
 }
---
+++ src/main/java/fr/m2i/javaspring/config/DevDataSourceConfig.java
@@ -0,0 +1,19 @@
+
+package fr.m2i.javaspring.config;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Profile("dev")
+public class DevDataSourceConfig implements DataSourceConfig {
+    
+    @Override
+    public void setup(){
+        System.out.println("Setting up datasource for DEV environment");
+        
+    }
+    
+    
+}
