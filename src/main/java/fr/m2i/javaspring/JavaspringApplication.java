dd9d9a01ca7d124a2ce17f675fb1386f35c7dbb2
a rahni <rahniahmed@gmail.com>
2022-08-29 15:32:15 +0200

    profile config via .properties files

--- src/main/java/fr/m2i/javaspring/JavaspringApplication.java
+++ src/main/java/fr/m2i/javaspring/JavaspringApplication.java
@@ -1,5 +1,42 @@
 package fr.m2i.javaspring;
 
+
+
+import fr.m2i.javaspring.config.DataSourceConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
+
+@SpringBootApplication
+public class JavaspringApplication {
+
+    public static void main(String[] args) {
+
+        ConfigurableEnvironment env = new StandardEnvironment();
+        env.setActiveProfiles("dev");
+
+        SpringApplication sa = new SpringApplication(JavaspringApplication.class);
+        sa.setEnvironment(env);
+
+        ApplicationContext ctx = sa.run(args);
+
+        DataSourceConfig dataConfig = ctx.getBean(DataSourceConfig.class);
+        dataConfig.setup();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
 import fr.m2i.javaspring.config.DataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +49,8 @@ public class JavaspringApplication {
 		ApplicationContext ctx = SpringApplication.run(JavaspringApplication.class, args);
                 
                 DataSourceConfig dataConfig = ctx.getBean(DataSourceConfig.class);
-                dataConfig.setup();
-	}
+                dataConfig.setup();	
+}
 
 }
+*/
